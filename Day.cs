d5b14bc27b402871bd57b23d6878809db162a6db
vvoudon <vvoudon@d-edge.com>
2023-12-04 13:53:04 +0100

    Add project files.

---
+++ Day.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+public abstract class Day
+{
+    protected abstract string testInput { get; }
+    protected abstract string input { get; }
+
+    public void Solve()
+    {
+        Run(input);
+    }
+
+    public void Test()
+    {
+        Run(testInput);
+    }
+
+    protected abstract void Run(string input);
+}
