89231cc622f3ea73f7e8780a0b01d83026c64e61
vvoudon <vvoudon@d-edge.com>
2023-12-06 21:46:37 +0100

    day 6

--- Day6_2.cs
+++ Day6_2.cs
@@ -5,9 +5,19 @@ internal class Day6_2 : Day
     protected override void Run(string input)
     {
         var inputLines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+        var raceTime = long.Parse(inputLines[0].Substring(6).Replace(" ", ""));
+        var raceTravelGoal = long.Parse(inputLines[1].Substring(9).Replace(" ", ""));
 
-
-        Console.WriteLine();
         
+            int oks = 0;
+            for (var holdTime = 1; holdTime <= raceTime; holdTime++)
+            {
+                if ((raceTime - holdTime) * holdTime > raceTravelGoal)
+                    oks++;
+                else if (oks > 1)
+                    break;
+            }
+
+        Console.WriteLine(oks);
     }
 }
